0b6b219a775fcab634e45b8cfaf0a288fe5f26fe
Adarsh <Adarsh@DESKTOP-29B47LM>
2021-05-09 09:48:41 +0530

    qa

--- Test Suites/adarshsuite.ts
+++ Test Suites/adarshsuite.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>a116aca9-40e5-4a75-a5f2-0ab6c0343bc1</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb7b96b7-eb55-4755-82dd-e0183d610f14</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login script</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
