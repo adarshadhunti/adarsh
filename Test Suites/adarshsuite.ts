778b1b60a33855e58cb1eac3d056435787ba1e52
Adarsh <Adarsh@DESKTOP-29B47LM>
2021-05-09 09:41:34 +0530

    qa

---
+++ Test Suites/adarshsuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>qa</description>
+   <name>adarshsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a116aca9-40e5-4a75-a5f2-0ab6c0343bc1</testSuiteGuid>
+</TestSuiteEntity>
